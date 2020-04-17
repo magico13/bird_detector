2095acf1320a099b03c5d06d94110fb5f7d2a89d
magico13 <magico1313@gmail.com>
2020-04-17 10:45:13 -0400

    Add some config to the top of detect_folder

--- detect_folder.py
+++ detect_folder.py
@@ -2,18 +2,18 @@ import cv2
 import os
 from pathlib import Path
 
+folder = 'burd3'
+mark = False
+x1 = 15
+x2 = 525
+y1 = 100
+y2 = 350
+
 bird_class = cv2.CascadeClassifier('cascade.xml')
-folder = 'bird_images'
+
 # Opening image 
 for im in os.listdir(folder):
     img = cv2.imread(f'{folder}/{im}')
-
-    x1 = 0
-    x2 = img.shape[1]
-    y1 = 100
-    y2 = 350
-    #y1 = 0
-    #y2 = img.shape[0]
     img_gray = cv2.cvtColor(img[y1:y2, x1:x2], cv2.COLOR_BGR2GRAY) 
 
     # Use minSize because we're not 
@@ -30,25 +30,19 @@ for im in os.listdir(folder):
         
         # There may be more than one 
         # birdin the image 
-        for (x, y, width, height) in found: 
-            # We draw a green rectangle around 
-            # every recognized bird 
+        if mark:
+            for (x, y, width, height) in found: 
+                # We draw a green rectangle around 
+                # every recognized bird 
 
-            #adjust for roi
-            x += x1
-            y += y1
+                #adjust for roi
+                x += x1
+                y += y1
 
-            cv2.rectangle(img, (x, y), 
-                        (x + height, y + width), 
-                        (0, 255, 0), 5) 
-            
-        # Creates the environment of 
-        # the picture and shows it 
-        #plt.subplot(1, 1, 1) 
-        #plt.imshow(img_rgb) 
-        #plt.show()
-        #with open(, 'wb') as f:
-            #plt.savefig(f)
+                cv2.rectangle(img, (x, y), 
+                            (x + height, y + width), 
+                            (0, 255, 0), 5) 
+                
         path = Path(im)
         cv2.imwrite(f'out/{path.stem}_{amount_found}{path.suffix}', img)
         
